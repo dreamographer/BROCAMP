daf692e05f942c46dd9c74f5fa7c1adbb5831e2c
Ashwin <ashwinkv.akv@gmail.com>
2023-11-28 20:50:55 +0530

    added music

--- main.js
+++ main.js
@@ -151,6 +151,23 @@ controls.autoRotate=true
 controls.autoRotateSpeed = 2; 
 
 
+// Adding audio
+// create an AudioListener and add it to the camera
+const listener = new THREE.AudioListener();
+camera.add( listener );
+
+// create a global audio source
+const sound = new THREE.Audio( listener );
+
+// load a sound and set it as the Audio object's buffer
+const audioLoader = new THREE.AudioLoader();
+audioLoader.load( '/sounds/interstellar..mp3', function( buffer ) {
+	sound.setBuffer( buffer );
+	sound.setLoop( true );
+	sound.setVolume( 1 );
+	sound.play();
+});
+
 
 window.addEventListener( 'resize', onWindowResize );
 
